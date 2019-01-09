4eee11640c41fb98550571330ee95582c05c3272
Max <chestm007@hotmail.com>
2019-01-09 23:49:50 +1100

    updating daemon test to include checking logs for expected messages

--- tests/mock_integration_tests/test_daemon.py
+++ tests/mock_integration_tests/test_daemon.py
@@ -17,10 +17,14 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
-import os
+import sys
+
+import logging
+from io import StringIO
 
 from mock import patch
 from base_test_object import BaseTestObject
+from linux_thermaltake_rgb import LOGGER
 from linux_thermaltake_rgb.daemon.daemon import ThermaltakeDaemon, Config
 from linux_thermaltake_rgb.devices.psus import ThermaltakePSUDevice
 
@@ -32,12 +36,27 @@ class DaemonMockIntegrationTest(BaseTestObject):
 
     @patch('linux_thermaltake_rgb.drivers.ThermaltakeControllerDriver._initialize_device', autospec=True)
     def test_basic_startup(self, init_dev):
-        daemon = ThermaltakeDaemon()
+        LOGGER.setLevel(logging.DEBUG)
+        stream = StringIO()
+        stream_handler = logging.StreamHandler(stream)
+        # stream_handler = logging.StreamHandler(sys.stdout)
+
+        LOGGER.addHandler(stream_handler)
+        try:
+            daemon = ThermaltakeDaemon()
+        finally:
+            stream_handler.flush()
+            LOGGER.removeHandler(stream_handler)
         self.assertIsNotNone(daemon)
         self.assertIsNotNone(daemon.config.controllers)
         self.assertTrue(init_dev.called)
         for psu in daemon.psus:
             self.assertIsInstance(psu, ThermaltakePSUDevice)
+
+        logging_output = stream.getvalue()
+        print(logging_output)
+        for keyword in ('ThermaltakePSUDevice', '** start **', '** end **'):
+            self.assertIn(keyword, logging_output)
 
     def tearDown(self):
         Config.abs_config_dir = str(self.config_abs_path)
